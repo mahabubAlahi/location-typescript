8ae9c8a3a5fea4f12fb9f56129a4d4cd0763ed20
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-06-26 06:35:19 +0600

    add: customMap class created

---
+++ src/CustomMaps.ts
@@ -0,0 +1,44 @@
+
+// Instructions to be a 'addMarker'
+export interface Mappable {
+
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    markerContent(): string;
+
+}
+
+export class CustomMaps {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string){
+        this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+            center: { lat: 0, lng: 0 },
+            zoom: 1,
+          });
+    }
+
+    addMarker(mappable: Mappable): void{
+
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
+
+        marker.addListener('click', () => {
+            const infowindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            })
+
+            infowindow.open(this.googleMap, marker)
+        })
+
+    }
+
+}
